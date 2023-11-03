ca8a868322b8a2e630ce3a75b3c348ea30f35345
sunjiaxing <13722807589@163.com>
2023-11-03 22:02:42 +0800

    premitter

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,7 +3,7 @@
  * @author: 观者
  * @lastEditors: 观者
  * @Date: 2023-10-31 17:42:48
- * @LastEditTime: 2023-11-03 21:54:25
+ * @LastEditTime: 2023-11-03 22:02:01
  */
 import { View, Text } from '@tarojs/components';
 import { useLoad } from '@tarojs/taro';
@@ -11,10 +11,10 @@ import './index.less';
 
 export default function Index() {
   useLoad(() => {
-    console.log('Page loaded.')
+    console.log('Page loaded.');
   });
 
-  console.log(1, "3", 5)
+  console.log(1, '3', 5);
 
   return (
     <View className="index">
