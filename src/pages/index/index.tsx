4896a844d477e662cedbb7e9eb2db9822c452c8c
sunjiaxing <13722807589@163.com>
2023-11-03 21:46:33 +0800

    prettier配置

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,24 @@
 /*
- * @description: 
+ * @description:
  * @author: 观者
  * @lastEditors: 观者
  * @Date: 2023-10-31 17:42:48
- * @LastEditTime: 2023-11-03 20:38:55
+ * @LastEditTime: 2023-11-03 21:45:48
  */
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.less'
+import { View, Text } from '@tarojs/components';
+import { useLoad } from '@tarojs/taro';
+import './index.less';
 
 export default function Index() {
-
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log('Page loaded.');
+  });
+
 
 
   return (
     <View className="index">
       <Text>Hello world!</Text>
     </View>
-  )
+  );
 }
